6e5c8eae9e08dd8b281f1c931584dc5ae4fccf2f
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-05-03 16:17:38 -0700

    Simple Method

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var Car = function (make, model) {
     this.make = make;
     this.model = model;
     "use strict"
-    window.console.log("The " +this.make + " " + this.model +" " + "car has rolled off of the assembly line")
+    window.console.log("The " + this.make + " " + this.model + " " + "car has rolled off of the assembly line")
 
 }
 
@@ -15,3 +15,19 @@ var jeep = new Car("Jeep", "CJ");
 
 //This shows the constructor, which is actually the function Car
 window.console.log(camaro.constructor);
+
+// Defining Public Memebers
+
+var Plane = function (type) {
+    "use strict"
+    this.type = type;
+    
+}
+// Create the method
+Plane.prototype.displayMake = function () {
+    "use strict"
+    window.console.log("You own a " + this.type + " plane");
+}
+var cub = new Plane("Carbon Cub");
+// Calling the method
+cub.displayMake();
