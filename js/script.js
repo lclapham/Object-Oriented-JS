7cbe49d4e5d12520469e2d5c40fa006f169bc5f2
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-05-03 16:07:30 -0700

    Part 2 completed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 /*eslint-env browser*/
 
-var Car = function () {
+var Car = function (make, model) {
+    this.make = make;
+    this.model = model;
     "use strict"
+    window.console.log("The " +this.make + " " + this.model +" " + "car has rolled off of the assembly line")
 
 }
+
+var camaro = new Car("Chevy", "Camaro");
+var corvette = new Car("Chevy", "Covertte");
+var dodge = new Car("Dodge", "Ram");
+var jeep = new Car("Jeep", "CJ");
+
+//This shows the constructor, which is actually the function Car
+window.console.log(camaro.constructor);
