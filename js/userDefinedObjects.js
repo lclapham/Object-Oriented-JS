650b17ae6274609049eea33c05f37e13346c4be9
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-05-09 12:27:57 -0700

    Finished.

--- js/userDefinedObjects.js
+++ js/userDefinedObjects.js
@@ -1,18 +1,34 @@
 // Object Literal Sytnax
 
-var Car =  {
-    "use strict"
+// var Car =  {
+//     "use strict"
+//     make: "Chevy",
+//     model: "Camaro",
+//     year: "2015"
+// };
+
+// // Object Constructor
+
+// function Car () {
+//     this.make = "Chevy";
+//     this.model = "Camaro";
+//     this.year = 2015;
+// }
+
+// var myCar = new Car();
+
+// Part 136
+
+var Car = {
     make: "Chevy",
-    model: "Camaro",
-    year: "2015"
-};
+    displayVehicleInfo: function(model, year) {
+        "use strict";
+        return (year + " " + this.make + " " + model);
 
-// Object Constructor
+    }
+    };
 
-function Car () {
-    this.make = "Chevy";
-    this.model = "Camaro";
-    this.year = 2015;
-}
+    window.console.log(Car.displayVehicleInfo("Camaro", "2015"));
+    window.console.log(Car.make);
 
-var myCar = new Car();
+    // This course is horrible.  The instructor goes way to fast and there is no direction.  Everything is too complex and never explained.
